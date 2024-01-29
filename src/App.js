7cb1b34d8c0b21f4d3ad0ff9fe39b1051e2d0923
Mohammad-Shahin23 <mohammad.shahin2221@gmail.com>
2024-01-29 14:03:36 +0000

    finished the slider need to fix the css

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Slider from "./components/Slider";
+
+function App() {
+  return (
+    <div className="container">
+      <Slider />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Slider.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { EffectCoverflow, Navigation } from 'swiper/modules';
+
+
+
+import '../styles/slider.css';
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+
+import 'swiper/css/navigation';
+
+
+
+import slide_image_1 from '../assets/images/img_1.jpg';
+import slide_image_2 from '../assets/images/img_2.jpg';
+import slide_image_3 from '../assets/images/img_3.jpg';
+import slide_image_4 from '../assets/images/img_4.jpg';
+import slide_image_5 from '../assets/images/img_5.jpg';
+import slide_image_6 from '../assets/images/img_6.jpg';
+import slide_image_7 from '../assets/images/img_7.jpg';
+
+function Slider() {
+  return (
+    <div className="container">
+      <h1 className="heading">Flower Gallery</h1>
+      <Swiper
+        effect={'coverflow'}
+        grabCursor={true}
+        centeredSlides={true}
+        loop={true}
+        slidesPerView={'auto'}
+        coverflowEffect={{
+          rotate: 0,
+          stretch: 0,
+          depth: 100,
+          modifier: 2.5,
+        }}
+        
+        navigation={{
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+          clickable: true,
+        }}
+        modules={[EffectCoverflow, Navigation]}
+        className="swiper_container"
+      >
+        <SwiperSlide>
+          <img src={slide_image_1} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_image_2} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_image_3} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_image_4} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_image_5} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_image_6} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_image_7} alt="slide_image" />
+        </SwiperSlide>
+
+        <div className="slider-controler">
+          <div className="swiper-button-prev slider-arrow">
+            <ion-icon name="arrow-back-outline"></ion-icon>
+          </div>
+          <div className="swiper-button-next slider-arrow">
+            <ion-icon name="arrow-forward-outline"></ion-icon>
+          </div>
+          <div className="swiper-pagination"></div>
+        </div>
+      </Swiper>
+    </div>
+  );
+}
+
+export default Slider;
